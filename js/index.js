0642eed7d336a62714c4906ada9b990a91ddb144
Ryan Westphal <ryan@trippingthebits.com>
2012-11-25 17:27:13 -0500

    Phantom image; copyright; menu fix;

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 
 */
 
-
-$(function() {
-
+$("body").on("pagebeforecreate", "[data-role='page']", function() {
+  if ( this.id != "index" ) {
+    $("#index .site-nav").clone(true).appendTo($(this).find("[data-role='content']"));
+    $("#index .ui-footer").clone(true).appendTo(this);
+  }
 });
